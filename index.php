cfb0bffdfcf00f3fe544fcccfbe21e7ebad91dc3
Rao Daniyal <raodaniyal1995@yahoo.com>
2021-11-18 02:29:48 +0500

    banner work

--- index.php
+++ index.php
@@ -9,15 +9,18 @@
 
 
     <div class="main-slider">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6">
-                    <h2>We integrates strategies for the ultimate digital impact.</h2>
-                    <p>We believe in taking new initiatives in the digital world. From development to integration, we are strong impact creators to offer maximum conversions and on-site engagement.</p>
-                    <a href="#" class='themeBtn'>Request a Quote</a>
-                </div>
-                <div class="col-md-6">
-                    <figure><img src="images/slideImg.png" alt=""></figure>
+        <img class="img-fluid w-100" src="images/hero-bg.png" alt="First slide">
+        <div class="carousel-caption">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <h2>We integrates strategies for the ultimate digital impact.</h2>
+                        <p>We believe in taking new initiatives in the digital world. From development to integration, we are strong impact creators to offer maximum conversions and on-site engagement.</p>
+                        <a href="#" class='themeBtn'>Request a Quote</a>
+                    </div>
+                    <div class="col-md-6">
+                        <figure><img src="images/slideImg.png" alt=""></figure>
+                    </div>
                 </div>
             </div>
         </div>
