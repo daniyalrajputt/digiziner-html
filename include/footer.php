33aebce347d33137a1ac4b4a727f3f890a20e0cd
Rao Daniyal <raodaniyal1995@yahoo.com>
2021-11-23 01:26:56 +0500

    minified css/js

--- include/footer.php
+++ include/footer.php
@@ -51,15 +51,6 @@
 </footer>
 <!-- END: Footer -->
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<!-- <script src="js/jquery-3.5.1.min.js"></script> -->
-<!-- <script src="js/bootstrap.min.js"></script> -->
-<!-- <script src="js/popper.min.js"></script> -->
-<!-- <script src="js/jquery.fancybox.min.js"></script> -->
-<!-- <script src="js/slick.min.js"></script> -->
-<!-- <script src="js/wow.min.js"></script> -->
-<!-- <script src="js/jquery.slimscroll.min.js"></script> -->
 <script src="js/all.min.js"></script>
 <script src="js/custom.min.js"></script>
 
